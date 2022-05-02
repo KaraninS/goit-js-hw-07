b472a46ee78dbb23843581f08e280d868d047f29
Karanin Serhii <36915523+KaraninS@users.noreply.github.com>
2022-05-02 15:15:55 +0300

    HW2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const itemsMarkup = galleryItems.map(({ preview, original, description}) => {
+  return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+}).join('');
+
+let instance;
+
+galleryList.insertAdjacentHTML('afterbegin', itemsMarkup);
+galleryList.addEventListener('click', onGalleryListClick);
+
+function onGalleryListClick(event) {
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    const url = event.target.dataset.source;
+    instance = createModal(url);
+  
+    instance.show(window.addEventListener('keydown', onEscClick));
+    };
+    
+};
+
+function createModal(url) {
+  return basicLightbox.create(`
+    <img src="${url}">
+  `,
+    {
+      onClose: () => {
+        window.removeEventListener('keydown', onEscClick);
+      }
+    }
+  );
+};
+
+function onEscClick(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const itemsMarkup = galleryItems.map(({ preview, original, description}) => {
+  return `
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`
+}).join('');
+
+galleryList.insertAdjacentHTML('afterbegin', itemsMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
